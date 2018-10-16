bff7c32e22ca7845dae4472d2af7f3a8996eeb57
mansilla <jaime.mansilla98@gmail.com>
2018-10-16 09:18:39 +0200

    actualizacion

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,48 +1,52 @@
-
 package juego;
 
 import java.util.Scanner;
 
 /**
  * Clase que contiene el juego del ahorcado
+ *
  * @author Mansilla jaime.mansilla98@gmail.com
  * @version 1.0
  */
 public class Juego {
 
-  
- public static void main(String[] args) {
-     //**********Variables*************
+    public static void main(String[] args) {
+        //**********Variables*************
         Scanner s = new Scanner(System.in);
-        int vidas = 6;
+        int vidas;
         String palabra;
-        int aciertos = 0;
+        int aciertos;
         int tamaño;
         String opcion;
-        int contador = 0;
+        int contador;
         String[] palabras = new String[]{"lapiz", "goma", "libreta", "maestro", "examen", "matematicas"};
         char[] respuesta;
-        palabra = palabras[(int) (Math.random() * 5)];
-        tamaño = palabra.length();
-        respuesta = new char[tamaño];
-         
-         
-        for (int i = 0; i < tamaño; i++) {
-            respuesta[i] = 'X';
-        }
-         
- //se ejecuta mientras no haya llegado a los aciertos o haya muerto
-        while (aciertos != tamaño && vidas != 0) {
-            System.out.println("=========AHORCADO :)==========          VIDAS="+vidas +" Aciertos= "+aciertos);
-            dibujar(vidas);
-            System.out.println("");
+        String r;
+        do {
+            vidas = 6;
+            contador = 0;
+            aciertos = 0;
+            palabra = palabras[(int) (Math.random() * 5)]; //aqui se escoge la palabra a adivinar
+            tamaño = palabra.length();
+            respuesta = new char[tamaño];
+
             for (int i = 0; i < tamaño; i++) {
- 
-                System.out.print(respuesta[i]);
+                respuesta[i] = 'X';
             }
-            System.out.println("\nIngresa una letra: ");
-            opcion = s.next();
-            if (palabra.contains(opcion)) {
+
+            //se ejecuta mientras no haya llegado a los aciertos o haya muerto
+            while (aciertos != tamaño && vidas != 0) {
+                System.out.println("=========AHORCADO :)==========          VIDAS=" + vidas + " Aciertos= " + aciertos);
+                dibujar(vidas);
+                System.out.println("");
+                for (int i = 0; i < tamaño; i++) {
+
+                    System.out.print(respuesta[i]);
+                }
+                System.out.println("\nIngresa una letra: ");
+                opcion = s.next();
+               
+               if (palabra.contains(opcion)) {
                 for (int i = 0; i < tamaño; i++) {
                     if (palabra.charAt(i) == opcion.charAt(0)) {
                         respuesta[i] = opcion.charAt(0);
@@ -57,36 +61,40 @@ public class Juego {
  
             contador = 0;
         }
-        if(vidas==0){
-            dibujar(vidas);
-        }else
-        {
-            System.out.println("");
-            for (int i = 0; i < tamaño; i++) {
- 
-                System.out.print(respuesta[i]);
+            if (vidas == 0) {
+                dibujar(vidas);
+            } else {
+                System.out.println("");
+                for (int i = 0; i < tamaño; i++) {
+
+                    System.out.print(respuesta[i]);
+                }
+                System.out.println(" ");
+                System.out.println("YOU WIN :)");
+
             }
-            System.out.println(" ");
-            System.out.println("YOU WIN :)");
-             
-        }
+            System.out.println("volver a jugar? (S/N)");
+            r = s.next();
+
+        } while (r.equalsIgnoreCase("S"));
     }
-    
-/**
- * Metodo que dibuja el muñeco en funcion de las vidas que queden
- * @param i indice que significa vidas 
- */ 
+
+    /**
+     * Metodo que dibuja el muñeco en funcion de las vidas que queden
+     *
+     * @param i indice que significa vidas
+     */
     private static void dibujar(int i) {
         switch (i) {
             case 6:
                 System.out.println(" ---------------------");
                 for (int j = 0; j < 15; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 break;
- 
+
             case 5:
                 System.out.println(" ---------------------");
                 System.out.println(" |                     |");
@@ -97,11 +105,11 @@ public class Juego {
                 System.out.println(" |                  -------");
                 for (int j = 0; j < 10; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 break;
- 
+
             case 4:
                 System.out.println(" ---------------------");
                 System.out.println(" |                     |");
@@ -117,11 +125,11 @@ public class Juego {
                 System.out.println(" |                     |   ");
                 for (int j = 0; j < 5; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 break;
- 
+
             case 3:
                 System.out.println(" ---------------------");
                 System.out.println(" |                     |");
@@ -137,11 +145,11 @@ public class Juego {
                 System.out.println(" |                     |   ");
                 for (int j = 0; j < 5; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 break;
- 
+
             case 2:
                 System.out.println(" ---------------------");
                 System.out.println(" |                     |");
@@ -157,11 +165,11 @@ public class Juego {
                 System.out.println(" |                     |   ");
                 for (int j = 0; j < 5; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 break;
- 
+
             case 1:
                 System.out.println(" ---------------------");
                 System.out.println(" |                     |");
@@ -178,15 +186,15 @@ public class Juego {
                 System.out.println(" |                    /  ");
                 System.out.println(" |                   /      ");
                 System.out.println(" |                  /       ");
-                for (int j = 0; j <2; j++) {
+                for (int j = 0; j < 2; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 break;
- 
+
             case 0:
-               System.out.println(" ---------------------");
+                System.out.println(" ---------------------");
                 System.out.println(" |                     |");
                 System.out.println(" |                     |");
                 System.out.println(" |                  -------");
@@ -203,7 +211,7 @@ public class Juego {
                 System.out.println(" |                  /     \\ ");
                 for (int j = 0; j < 2; j++) {
                     System.out.println(" |");
- 
+
                 }
                 System.out.println("__________");
                 System.out.println("GAME OVER");
